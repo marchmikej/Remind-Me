0fbb4ffcf79687d60f4f2bb4768ba1c576804f0e
mjmarch33333 <marchmikej@gmail.com>
2016-01-09 07:31:35 -0500

    Added sending to safari and notifications
    Added sending to safari and notifications

--- Remind Me/AppDelegate.swift
+++ Remind Me/AppDelegate.swift
@@ -13,7 +13,8 @@ import SafariServices
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    // View controller instance, this is the hook to your ViewController
+    var vc = ViewController()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,10 +32,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
         
-        let deviceTokenString = NSString(format: "%@", deviceToken) as String
+        var deviceTokenString = NSString(format: "%@", deviceToken) as String
         
-        print(deviceTokenString)
-        print("----------")
+        deviceTokenString=deviceTokenString.stringByReplacingOccurrencesOfString(" ", withString: "")
+        deviceTokenString=deviceTokenString.stringByReplacingOccurrencesOfString("<", withString: "")
+        deviceTokenString=deviceTokenString.stringByReplacingOccurrencesOfString(">", withString: "")
+
         let defaults = NSUserDefaults.standardUserDefaults()
         defaults.setObject(deviceTokenString, forKey: "token")
     }
@@ -45,37 +48,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
         // This is called when the app is running and a notification is received
-        // If app is not in foreground nothing seems to happen
-        print("Hi Mike I sent you something!!!1")
-        if application.applicationState == UIApplicationState.Inactive || application.applicationState == UIApplicationState.Background {
-            //opened from a push notification when the app was on background
-            print("from outside app")
+        // If app is not in foreground nothing seems to happen        print("Hi Mike I sent you something!!!1")
+        print("Recived: \(userInfo)")
+        //Parsing userinfo:
+        var temp : NSDictionary = userInfo
+        if let info = userInfo["aps"] as? Dictionary<String, AnyObject>
+        {
+
+        }
+        if let info = userInfo["aps"] as? Dictionary<String, AnyObject>
+        {
+            let urlMessage = info["url"] as! String
+            print("urlMessage");
+            print(urlMessage);
+            if application.applicationState == UIApplicationState.Inactive || application.applicationState == UIApplicationState.Background {
+                //opened from a push notification when the app was on background
+                print("from outside app")
+                let defaults = NSUserDefaults.standardUserDefaults()
+                defaults.setObject(urlMessage, forKey: "forwardurl")
+            } else {
+                print("from inside app")
+                //vc.showMessageReceivedAlert()
+                let alertMsg = info["alert"] as! String
+                var alert: UIAlertView!
+                alert = UIAlertView(title: "", message: alertMsg, delegate: nil, cancelButtonTitle: "OK")
+                alert.show()
+            }
         }
     }
     
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        print("exiting1?")
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("exiting2?")
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         // Resets badge number when you go to app
-        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
-    }
+        print("starting1?")    }
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
+        print("starting?")
+        vc.shouldForward()
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("dying1?")
     }
-
 }
 
--- Remind Me/ViewController.swift
+++ Remind Me/ViewController.swift
@@ -11,10 +11,13 @@ import SafariServices
 
 class ViewController: UIViewController {
     
-    private var urlString:String = "http://rootedindezign.com"
+    private var urlString:String = "https://www.tenantsyncdev.com"
+    @IBOutlet var respondButtonMessage: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("viewcontroller 2")
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,26 +27,114 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        //let defaults = NSUserDefaults.standardUserDefaults()
+        //let token = defaults.objectForKey("token") as? String ?? String()
+        //if token.isEmpty {
+         //   print("Device Token not available")
+        //    print(token)
+        //} else {
+        //    print(token)
+       // }
+        print("viewcontroller 1")
+    }
+    
+    @IBAction func handleRespondToMessageButton() {
+        print("responding to users response")
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let urlforward = defaults.objectForKey("forwardurl") as? String ?? String()
+        let sendURL:String = urlString + "/" + urlforward;
+        print("going to url")
+        print(sendURL)
+        let svc = SFSafariViewController(URL: NSURL(string: sendURL)!)
+        self.presentViewController(svc, animated: true, completion: nil)
+    }
+    
+    @IBAction func handleNotificationRegistration()  {
+        NSLog("handleNotificationRegistration")
         let defaults = NSUserDefaults.standardUserDefaults()
         let token = defaults.objectForKey("token") as? String ?? String()
-        if token.isEmpty {
-            print("Device Token not available")
-            print(token)
-        } else {
-            print(token)
+        let sendURL:String = urlString + "/api/managenotifications/" + token;
+        print(sendURL);
+        let svc = SFSafariViewController(URL: NSURL(string: sendURL)!)
+        self.presentViewController(svc, animated: true, completion: nil)
+        /*
+        //var myUrl = NSURL(string: "https://www.tenantsyncdev.com/api/registeriapp?email=john@gmail.com&password=hhhhhh&routeId=mikesentagain&type=0");
+        var myUrl = NSURL(string: "https://www.tenantsyncdev.com/api/test");
+        let request = NSMutableURLRequest(URL:myUrl!);
+        request.HTTPMethod = "POST";
+        // Compose a query string
+        let postString = "email=john@gmail.com&password=hhhhhh&type=0&routeId=" + token;
+        print(postString)
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            // You can print out response object
+            print("response = \(response)")
+            // Print out response body
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("responseString = \(responseString)")
+            if ((responseString?.containsString("successful")) != false) {
+                print("we got it")
+            } else {
+                print("no go")
+            }
         }
-        var count: Int = defaults.integerForKey("count")
-        count++
-        defaults.setInteger(count, forKey: "count")
-        print(defaults.integerForKey("count"))
+        
+        task.resume()
+*/
+    }
+
+    func goToSite()  {
+        NSLog("goToSite")
         openWithSafariVC("hi")
+    }
+
+    func goToURL(urlForward: String)  {
+        print("goToURL")
+        print(urlForward)
+        UIApplication.sharedApplication().openURL(NSURL(string: self.urlString)!)
+    }
+    
+    func shouldForward() {
+        print("In should forward")
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let urlforward = defaults.objectForKey("forwardurl") as? String ?? String()
+
+        if(urlforward != "noforward" && !urlforward.isEmpty) {
+            print("forwarding to site")
+            defaults.setObject("noforward", forKey: "forwardurl")
+            let sendURL:String = urlString + "/" + urlforward;
+            print(sendURL)
+            UIApplication.sharedApplication().openURL(NSURL(string: sendURL)!)
+        } else {
+            print("not forwarding to site")
+        }
+    }
+    
+    func showMessageReceivedAlert() {
+        let alertController = UIAlertController(title: "New Message", message: "Here is your message", preferredStyle: .Alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
     }
     
     @IBAction func openWithSafariVC(sender: AnyObject)
     {
-        let svc = SFSafariViewController(URL: NSURL(string: self.urlString)!)
-        self.presentViewController(svc, animated: true, completion: nil)
+        print("opening the url with safarivc")
+        //Holding off sarfaiViewController for now
+        //let svc = SFSafariViewController(URL: NSURL(string: self.urlString)!)
+        //self.presentViewController(svc, animated: true, completion: nil)
+        UIApplication.sharedApplication().openURL(NSURL(string: self.urlString)!)
     }
-
 }
 
